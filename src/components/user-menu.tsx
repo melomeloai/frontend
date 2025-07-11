42532e10bb851657c4539c1f58b7b76f2c11a1b6
Galileo <galileooliveira90@gmail.com>
2025-07-11 16:18:05 -0700

    feat(init): init project

---
+++ src/components/user-menu.tsx
@@ -0,0 +1,39 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+interface User {
+  name?: string;
+  avatarUrl?: string;
+  email?: string;
+}
+
+interface UserMenuProps {
+  user: User;
+  onLogout: () => void;
+  onManagePlan: () => void;
+}
+
+export function UserMenu({ user, onLogout, onManagePlan }: UserMenuProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="cursor-pointer">
+          <AvatarImage
+            src={user.avatarUrl || "https://via.placeholder.com/40"}
+            alt="User avatar"
+          />
+          <AvatarFallback>{user.name?.[0] || "U"}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={onManagePlan}>Manage Plan</DropdownMenuItem>
+        <DropdownMenuItem onClick={onLogout}>Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
